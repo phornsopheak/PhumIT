f5c0a1ab442e20404c1536f0b339e4013b3e0c6a
Phorn Sopheak <phornsopheak17@gmail.com>
2016-07-19 20:33:03 +0700

    change css version 3

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users, path: "", path_names: { sign_in: "@dminphumitlog_in" }
   root "static_page#home"
   get "/about_us", to: "static_page#about_us"
-  get "/google8f8e10d7f43bf086", to: "static_page#google8f8e10d7f43bf086"
 
   resources :categories, only: [:show]
   resources :lists, only: [:show]
