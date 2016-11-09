d35a803cb60ccb8fda8acceecb187782955eb9e5
phornsopheak <phornsopheak17@gmail.com>
2016-11-09 09:14:16 +0700

    testing new function

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -5,6 +5,6 @@ class StaticPageController < ApplicationController
 
   def home
     @popular_articles = Article.unscoped.where(state: 0).order(view: :desc).limit(4)
-    @articles = @q.result.where(state: 0).page(params[:page]).per(10)
+    @articles = @q.result.where(state: 0).page(params[:page]).per(17)
   end
 end
