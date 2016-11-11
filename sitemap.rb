958062d95a19219c2c9010a1941a14ba289ead59
phornsopheak <phornsopheak17@gmail.com>
2016-11-11 14:21:38 +0700

    change font size

--- sitemap.rb
+++ sitemap.rb
@@ -1,9 +1,16 @@
 require 'rubygems'
 require 'sitemap_generator'
 
-SitemapGenerator::Sitemap.default_host = 'http://example.com'
+SitemapGenerator::Sitemap.default_host = 'http://www.phumit.com'
 SitemapGenerator::Sitemap.create do
   add '/home', :changefreq => 'daily', :priority => 0.9
-  add '/contact_us', :changefreq => 'weekly'
+  add '/about_us', :changefreq => 'weekly'
+  add '/category/1', :changefreq => 'weekly'
+  add '/category/2', :changefreq => 'weekly'
+  add '/category/3', :changefreq => 'weekly'
+  add '/category/4', :changefreq => 'weekly'
+  add '/category/5', :changefreq => 'weekly'
+  add '/category/6', :changefreq => 'weekly'
+  add '/category/7', :changefreq => 'weekly'
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
